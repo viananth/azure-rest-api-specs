feb32ac210c2cea7f9eae215409bb0eac8b08099
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-03-13 15:42:32 -0700

    Fixed some issues

--- specification/azsadmin/resource-manager/compute/doit.sh
+++
@@ -1,9 +0,0 @@
-set -v
-SDK_FOLDER=SDK
-NAME="Compute"
-DEST=/e/github/azure-sdk-for-net/src/AzureStack/${NAME}Admin/${NAME}.Admin
-rm  $SDK_FOLDER -rf
-autorest readme.md --csharp --csharp-sdks-folder=$SDK_FOLDER
-rm $DEST/Generated -rf
-mv  $SDK_FOLDER/${NAME}/${NAME}.Admin/Generated $DEST
-rm $SDK_FOLDER -rf
